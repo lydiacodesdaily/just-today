d1524b798302a360c35291c78cf4d52f20ad0534
Lydia Kwag <lydia.kwag.dev@gmail.com>
2025-12-27 18:02:55 -0500

    fix: update jest config, reinstalled

--- jest.setup.js
+++ jest.setup.js
@@ -3,3 +3,14 @@
 jest.mock('@react-native-async-storage/async-storage', () =>
   require('@react-native-async-storage/async-storage/jest/async-storage-mock')
 );
+
+// Mock Expo runtime to fix "import outside scope" error
+global.__ExpoImportMetaRegistry = {
+  register: jest.fn(),
+  get: jest.fn(() => ({})),
+};
+
+// Mock structuredClone for Expo
+if (typeof global.structuredClone === 'undefined') {
+  global.structuredClone = (obj) => JSON.parse(JSON.stringify(obj));
+}
